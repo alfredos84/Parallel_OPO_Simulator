c05cf278638d62355b5bab761e9dd19586d1a6bf
Alfredo Daniel Sanchez <45202555+alfredos84@users.noreply.github.com>
2024-09-10 10:34:05 +0200

    Update DtypesConsts.cuh

--- scr/headers/DtypesConsts.cuh
+++ scr/headers/DtypesConsts.cuh
@@ -26,10 +26,10 @@ using cVecd_t = thrust::device_vector<complex_t>;
 
 
 // Define global constants
-const uint SIZE   = 1 << 14;  // vector size
-const uint NZ     = 150;		  // size discretization
-const uint NRT    = 10000;		// number of round trips    
-const uint BLKX   = 1 << 7;		// block dimensions for kernels
+const uint SIZE   = 1 << 14;   // vector size
+const uint NZ     = 150;       // size discretization
+const uint NRT    = 10000;     // number of round trips    
+const uint BLKX   = 1 << 7;    // block dimensions for kernels
 
 const real_t PI   = 3.14159265358979323846;		  // pi
 const real_t C    = 299792458*1E6/1E12;		    	// speed of ligth in vacuum [um/ps]
